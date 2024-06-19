69192f087b96819070e23ef1c59f8fefa07af01c
Duy Le <duyanhle2906@gmail.com>
2024-06-19 15:32:27 +1000

    build: Add seperate frameworks for PresentationLayer, DataLayer and DomainLayer; configure dependencies between the layers

---
+++ DomainLayer/DomainLayer/DomainLayer.h
@@ -0,0 +1,18 @@
+//
+//  DomainLayer.h
+//  DomainLayer
+//
+//  Created by Duy Le on 19/6/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DomainLayer.
+FOUNDATION_EXPORT double DomainLayerVersionNumber;
+
+//! Project version string for DomainLayer.
+FOUNDATION_EXPORT const unsigned char DomainLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DomainLayer/PublicHeader.h>
+
+
