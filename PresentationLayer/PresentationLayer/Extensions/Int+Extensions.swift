4edd3c76ba3ea94c042afdf42c07d9ffda258fd1
Duy Le <duyanhle2906@gmail.com>
2024-06-19 21:22:16 +1000

    refactor[Presentation]: Improve the int extensions to display time string

--- PresentationLayer/PresentationLayer/Extensions/Int+Extensions.swift
+++ PresentationLayer/PresentationLayer/Extensions/Int+Extensions.swift
@@ -10,9 +10,15 @@ import Foundation
 extension Int {
     func toTimeString() -> String {
         var timeString = "\(self)s"
-        if abs(self) > 60 {
+        // if the time is larger than 5 minutes
+        if abs(self) > 60 * 5 {
             timeString = "\(self / 60)m"
+        } else if abs(self) > 60 { // the time is less than 5 minutes but larger 1 minute
+            let minutes = Int(self / 60)
+            let seconds = Int(abs(self) % 60)
+            timeString = "\(minutes)m \(seconds)s"
         }
+        
         return timeString
     }
 }
