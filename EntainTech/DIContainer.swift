c6dcd7bc4827ca0a23c828de032539626cf07427
Duy Le <duyanhle2906@gmail.com>
2024-06-20 09:53:37 +1000

    feat[App]: Implement DIContainer

---
+++ EntainTech/DIContainer.swift
@@ -0,0 +1,42 @@
+//
+//  DIContainer.swift
+//  EntainTech
+//
+//  Created by Duy Le on 19/6/2024.
+//
+
+import Foundation
+import Swinject
+import PresentationLayer
+import DomainLayer
+import DataLayer
+
+extension Container {
+    
+    static let sharedContainer: Container = {
+        let container = Container()
+        
+        container.register(ApiServiceProtocol.self) { _ in
+            ApiService()
+        }
+        
+        container.register(RaceRemoteDataSourceProtocol.self) { _ in
+            RaceRemoteDataSource(apiService: container.resolve(ApiServiceProtocol.self)!)
+        }
+        
+        container.register(RaceRepoProtocol.self) { _ in
+            RaceRepo(remoteDatasource: container.resolve(RaceRemoteDataSourceProtocol.self)!)
+        }
+        
+        container.register(RaceInteractorProtocol.self) { _ in
+            RaceInteractor(repo: container.resolve(RaceRepoProtocol.self)!)
+        }
+        
+        container.register(RaceListViewModel.self) { _ in
+            RaceListViewModel(interactor: container.resolve(RaceInteractorProtocol.self)!)
+        }
+        
+        return container
+    }()
+    
+}
