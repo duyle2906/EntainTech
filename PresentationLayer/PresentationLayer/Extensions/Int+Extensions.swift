f8d3d69f977b4c68e6114eb1bc4c704f3498e5dd
Duy Le <duyanhle2906@gmail.com>
2024-06-20 12:57:53 +1000

    refactor[Presentation]: Refactor the logic of converting Int to time string

--- PresentationLayer/PresentationLayer/Extensions/Int+Extensions.swift
+++ PresentationLayer/PresentationLayer/Extensions/Int+Extensions.swift
@@ -9,16 +9,18 @@ import Foundation
 
 extension Int {
     func toTimeString() -> String {
+        let timePoint = 5
         var timeString = "\(self)s"
         // if the time is larger than 5 minutes
-        if abs(self) > 60 * 5 {
-            timeString = "\(self / 60)m"
+        if abs(self) > 60 * timePoint {
+            timeString = "\(self / 60 + (self / abs(self)))m"
+        } else if abs(self) == 60 * timePoint {
+            timeString = "\(self / abs(self) * timePoint)m"
         } else if abs(self) > 60 { // the time is less than 5 minutes but larger 1 minute
             let minutes = Int(self / 60)
             let seconds = Int(abs(self) % 60)
             timeString = "\(minutes)m \(seconds)s"
         }
-        
         return timeString
     }
 }
